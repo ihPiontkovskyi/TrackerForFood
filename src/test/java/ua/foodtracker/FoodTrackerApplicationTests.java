31e272ac2b49e1e6a0cce6e4485ad7a6ba52b0c6
ihPiontkovskyi <fuvkaperviy@gmail.com>
2020-02-07 18:50:05 +0200

    InitialCommit

---
+++ src/test/java/ua/foodtracker/FoodTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.foodtracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FoodTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
