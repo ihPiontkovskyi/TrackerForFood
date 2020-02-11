d69c0352e9cb88353b590ac99d5d812ffc965a3d
ihPiontkovskyi <fuvkaperviy@gmail.com>
2020-02-11 15:56:30 +0200

    Fix password encoder

--- src/main/java/ua/foodtracker/config/AppConfiguration.java
+++ src/main/java/ua/foodtracker/config/AppConfiguration.java
@@ -2,6 +2,8 @@ package ua.foodtracker.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
@@ -21,6 +23,11 @@ public class AppConfiguration implements WebMvcConfigurer {
         LocaleChangeInterceptor localeChangeInterceptor = new LocaleChangeInterceptor();
         localeChangeInterceptor.setParamName("lang");
         return localeChangeInterceptor;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
     @Override
